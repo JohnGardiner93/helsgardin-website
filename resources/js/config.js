1e0b82f107b926dc74d27dc8802a587aa6230459
JohnGardiner93 <john.gardiner93@gmail.com>
2021-07-06 05:28:51 -0400

    Section Added - Title
    Added title section to site. Includes slideshow component with buttons.
    Next step will be to add timer to slideshow.

---
+++ resources/js/config.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// SECTION - TITLE
+export const TITLE_SLIDES_NUM = 3;
+export const TITLE_STARTING_SLIDE = 1;
---
+++ resources/js/title.js
@@ -0,0 +1,58 @@
+"use strict";
+////////////////////////////////////////////
+// Imports
+import { TITLE_SLIDES_NUM, TITLE_STARTING_SLIDE } from "./config.js";
+
+////////////////////////////////////////////
+// Elements
+const title = document.querySelector(`.section-title`);
+const slides = title.querySelectorAll(`.title-slide`);
+let curSlideNum = TITLE_STARTING_SLIDE;
+
+////////////////////////////////////////////
+// Functions
+
+/**
+ * Changes the slide being displayed to a new slide. Slideshow will move in standard or reverse numerical order depending on the provided input.
+ * @param {Boolean} [incrementUp=true] - Variable that controls the direction of the slide progression. If the value is true, the slides will progress in numerical order until the last slide is reached, at which point the slides will restart at the lowest number.
+ */
+const changeSlide = function (incrementUp = true) {
+  // Controls number increment direction. If incrementDirection = 1 (incrementUp = false), slides will go in reverse numerical order.
+  const incrementDirection = Number(!incrementUp);
+
+  const newSlideNum =
+    ((curSlideNum + incrementDirection) % TITLE_SLIDES_NUM) + 1;
+
+  slides.forEach((slide) => {
+    showAndHideSlide(slide, curSlideNum, newSlideNum);
+    return;
+  });
+  curSlideNum = newSlideNum;
+};
+
+/**
+ * Adds and removes required attributes/classes in order to show and hide the appropriate slides.
+ * @param {Element} slide - The element whose state will be adjusted (shown, hidden, or ignored).
+ * @param {Number} curNum - The current slide number. Element will be hidden if it's data-slide attribute is equal to this number.
+ * @param {Number} newNum - The new slide number. Element will be shown if it's data-slide attribute is equal to this number.
+ */
+const showAndHideSlide = function (slide, curNum, newNum) {
+  if (Number(slide.dataset.slide) === newNum) {
+    slide.classList.remove(`js--title-hidden`);
+  }
+  if (Number(slide.dataset.slide) === curNum) {
+    slide.classList.add(`js--title-hidden`);
+  }
+};
+
+////////////////////////////////////////////
+// Event Listeners
+title.addEventListener(`click`, function (e) {
+  const targetIsLeft = e.target.classList.contains(`js--btn-title-left`);
+  const targetIsRight = e.target.classList.contains(`js--btn-title-right`);
+
+  // Guard Clause. Action only occurs if one of the buttons are clicked.
+  if (!targetIsLeft && !targetIsRight) return;
+
+  changeSlide(targetIsRight);
+});
