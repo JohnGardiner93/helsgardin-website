c71bf6f83d329a5b4c53cef6b5f19e8ee04d7909
JohnGardiner93 <john.gardiner93@gmail.com>
2021-07-06 06:34:38 -0400

    Section Edit - Title - Add Timer
    Add a timer component to the slideshow. Timer starts when page
    initializes. Progresses to next slide on set interval (set in
    config.js). Progressing slideshow manually resets timer.

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,5 +1,6 @@
 "use strict";
 
 // SECTION - TITLE
-export const TITLE_SLIDES_NUM = 3;
-export const TITLE_STARTING_SLIDE = 1;
+export const TITLE_SLIDES_NUM = 3; // Number of slides in the slideshow
+export const TITLE_STARTING_SLIDE = 1; // Slide that first presents when the page is loaded
+export const TITLE_SLIDESHOW_TIME = 5000; // Time between switching to new slides in the slideshow (in ms)
